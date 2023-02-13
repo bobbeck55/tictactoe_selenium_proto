9648fa8a3f960b39113b4c610bbe46713318a609
bobbeck55 <bob_beck@sil.org>
2023-02-13 10:27:01 -0500

    demo script updates

--- run_demo.py
+++ run_demo.py
@@ -1,44 +1,52 @@
 #
 #
 #  Run demo - selenium test of tictactoe react app
-#  argv[1] - percy -> run with percy
-#  argv[2] - PERCY_BRANCH=master|local
-#  argv[3] - PERCH_TOKEN=<from percy.io>
+#  argv[1] - 'all' or test name
+#  argv[2] - percy -> run with percy
+#  argv[3] - PERCY_BRANCH=master|local
+#  argv[4] - PERCH_TOKEN=<from percy.io>
 #
 #
 import subprocess
 import sys
 import os
 
-if len(sys.argv) != 1 and len(sys.argv) != 4:
+if len(sys.argv) != 2 and len(sys.argv) != 5:
     print("Unsupported number of arguments: ", len(sys.argv))
     sys.exit(-1)
 
 myEnv = os.environ
-if len(sys.argv) == 4:
-    myEnv["PERCY_BRANCH"] = sys.argv[2]
-    myEnv["PERCY_TOKEN"] = sys.argv[3]
+if len(sys.argv) == 5:
+    myEnv["PERCY_BRANCH"] = sys.argv[3]
+    myEnv["PERCY_TOKEN"] = sys.argv[4]
 
 
 # spawn a new cmd prompt window with "npm start"
 p = subprocess.Popen([r'start', r'cmd', '/k', r'demo_cmd.bat'], shell=True)
 
-
 arg3 = r'dotnet'
 arg4 = r'test'
-arg5 = r'--no-restore'
+
+if sys.argv[1] == 'all':
+    arg5 = arg6 = ''
+else:
+    arg5 = "--filter"
+    arg6 = "Name=" + sys.argv[1]
 
 
-if len(sys.argv) == 4 and sys.argv[1] == 'percy':
+arg7 = r'--no-restore'
+
+
+if len(sys.argv) == 5 and sys.argv[2] == 'percy':
     cmd = r'node_modules\\.bin\\percy'
     arg1 = r'exec'
     arg2 = r'--'
 
-    subprocess.Popen([cmd, arg1, arg2, arg3, arg4, arg5], env=myEnv, shell=True)
+    subprocess.Popen([cmd, arg1, arg2, arg3, arg4, arg5, arg6, arg7], env=myEnv, shell=True)
 
 else:
 
-    subprocess.Popen([arg3, arg4, arg5], env=myEnv, shell=True)
+    subprocess.Popen([arg3, arg4, arg5, arg6, arg7], env=myEnv, shell=True)
 
 
 
