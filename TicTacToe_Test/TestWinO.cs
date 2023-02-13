f16a18325c690f55c790e4ceab587a1d86d73f8b
bobbeck55 <bob_beck@sil.org>
2023-02-13 09:10:42 -0500

    moved each test into its own file

---
+++ TicTacToe_Test/TestWinO.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Boa.Constrictor.Selenium;
+using Boa.Constrictor.Screenplay;
+using PercyIO.Selenium;
+
+namespace TicTacToe_Test
+{
+    partial class TicTacToe_Test
+    {
+        [Test]
+        public void TestWinO()
+        {
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> li = driver.FindElements(By.XPath(BoardPage.SquaresCollection));
+
+            li[1].Click();
+            li[2].Click();
+            li[0].Click();
+            li[5].Click();
+            li[4].Click();
+            li[7].Click();
+            li[6].Click();
+            li[8].Click();
+
+            Actor.WaitsUntil(Appearance.Of(GamePage.Status), IsEqualTo.True());
+            string text = Actor.AsksFor(Text.Of(GamePage.Status));
+            Assert.That(text, Is.EqualTo("Winner: O"));
+            Percy.Snapshot(driver, "Winner: O");
+        }
+    }
+}
---
+++ TicTacToe_Test/TestWinX.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Boa.Constrictor.Selenium;
+using Boa.Constrictor.Screenplay;
+using PercyIO.Selenium;
+
+namespace TicTacToe_Test
+{
+    partial class TicTacToe_Test
+    {
+        [Test]
+        public void TestWinX()
+        {
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> li = driver.FindElements(By.XPath(BoardPage.SquaresCollection));
+
+            li[2].Click();
+            li[4].Click();
+            li[8].Click();
+            li[7].Click();
+            li[5].Click();
+
+            Actor.WaitsUntil(Appearance.Of(GamePage.Status), IsEqualTo.True());
+            string text = Actor.AsksFor(Text.Of(GamePage.Status));
+            Assert.That(text, Is.EqualTo("Winner: X"));
+
+            Percy.Snapshot(driver, "Winner: X");
+        }
+    }
+}
